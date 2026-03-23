2478735309da61768b9ab0180eb408d1434df123
Goddii <otienogodwin23@gmail.com>
2026-03-23 12:16:57 +0300

    fix:fix for loop item to start at 0

--- discountCalculator.js
+++ discountCalculator.js
@@ -1,17 +1,20 @@
+
 function calculateDiscountedPrice(quantity, pricePerItem) {
     let totalPrice = 0;
-    debugger
-    for (let i = 1; i < quantity; i++) {
+    for (let i = 0; i < quantity; i++) {
+        debugger
         totalPrice += pricePerItem;
     }
+    debugger
 
     if (quantity >= 10) {
         totalPrice *= 0.9;
     }
 
     return totalPrice;
+    debugger
 }
-
+console.log(calculateDiscountedPrice(5,10))
 
 
 module.exports = calculateDiscountedPrice;
